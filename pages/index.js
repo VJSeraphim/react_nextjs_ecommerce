9f8de30b0d6ddb4088ab980d061fbf4c25a30ecd
VJS <minsung_96@naver.com>
2022-10-25 00:24:45 +0900

    JSX structure files create

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,27 @@
 import React from 'react'
 
-const index = () => {
+const Home = () => {
   return (
-    <div>
-      index
-    </div>
+    <>
+      HeroBanner
+
+      <div className="products-heading">
+        <h2>
+          Selling Products
+        </h2>
+        <p>
+          Speakers of many Variations
+        </p>
+
+        <div className="products-container">
+          {['Product 1', 'Product 2'].map((product) => product)}
+        </div>
+
+      </div>
+
+      Footer
+    </>
   )
 }
 
-export default index
+export default Home
