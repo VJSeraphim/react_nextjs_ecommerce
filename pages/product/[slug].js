ebea417c60aaad5c90b78a31c5904b3cc604d8dd
VJS <minsung_96@naver.com>
2022-11-01 23:58:51 +0900

    Product Query

---
+++ pages/product/[slug].js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import { client, urlFor } from '../../lib/client'
+
+const ProductDetails = () => {
+    return (
+        <div>
+            <div className="product-detail-container">
+                <div>
+                    <div className="image-container">
+                        <img 
+                            src=""
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProductDetails
