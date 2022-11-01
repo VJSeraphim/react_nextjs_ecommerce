07eeed1d732471554a5ce9436df44650b741f9b7
VJS <minsung_96@naver.com>
2022-11-01 23:59:17 +0900

    Product Query - staticProps

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -2,14 +2,16 @@ import React from 'react'
 
 import { client, urlFor } from '../../lib/client'
 
-const ProductDetails = () => {
+const ProductDetails = ({ product, products }) => {
+    const { image, name, details, price } = product
+
     return (
         <div>
             <div className="product-detail-container">
                 <div>
                     <div className="image-container">
                         <img 
-                            src=""
+                            src={urlFor(image && image[index])}
                         />
                     </div>
                 </div>
@@ -17,5 +19,16 @@ const ProductDetails = () => {
         </div>
     )
 }
+
+export const getStaticProps = async({ params: { slug }}) => {
+    const query = `*[_type == "product" && slug.current == '${slug}'][0]`
+    const productsQuery = `*[_type == "product"]`
+    const product = await client.fetch(query)
+    const products = await client.fetch(productsQuery)
+    
+    return {
+      props: { products, product }
+    }
+  }
 
 export default ProductDetails
