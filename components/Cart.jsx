68732c9c218a17302b2fb60a45ecefcbb0badf92
VJS <minsung_96@naver.com>
2022-11-06 23:58:59 +0900

    Shopping Cart Page

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import Link from 'next/link'
+import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from 'react-icons/ai'
+import { TiDeleteOutline } from 'react-icons/ti'
+import { Toast } from 'react-hot-toast'
+
+import { useStateContext } from '../context/StateContext'
+import { urlFor } from '../lib/client'
 
 const Cart = () => {
+    const cartRef = useRef()
+    const { totalPrice, totalQuantities, cartItems, setShowCart } = useStateContext()
     return (
-        <div>
-            Cart
+        <div className="cart-wrapper" ref={cartRef}>
+            <div className="cart-container">
+                <button
+                    type="button"
+                    className="cart-heading"
+                    onClick={() => setShowCart(false)}
+                >
+                    <AiOutlineLeft />
+                    <span className="heading">
+                        Your Cart
+                    </span>
+                    <span className="cart-num-items">
+                        ({totalQuantities} items)
+                    </span>
+                </button>
+                {cartItems.length < 1 && (
+                    <div className="empty-cart">
+                        <AiOutlineShopping 
+                            size={150}
+                        />
+                        <h3>
+                            Your Shopping Cart is Empty.
+                            <Link href="/">
+                                <button 
+                                    type="button" 
+                                    onClick={() => setShowCart(false)}
+                                    className="btn"
+                                >
+                                    Continue Shopping
+                                </button>
+                            </Link>
+                        </h3>
+                    </div>
+                )}
+                <div className="product-container">
+                    {cartItems.length >= 1 && cartItems.map((item, i) => (
+                        <div className="product" key={item._id}>
+                            <img 
+                                src={urlFor(item?.image[0])}
+                                className="cart-product-image"
+                            />
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import Link from 'next/link'
 import { AiOutlineShopping } from 'react-icons/ai'
 
+import { Cart } from './'
+import { useStateContext } from '../context/StateContext'
+
 const Navbar = () => {
+    const { showCart, setShowCart, totalQuantities } = useStateContext()
+
     return (
         <div className="navbar-container">
             <p className="logo">
@@ -10,12 +15,13 @@ const Navbar = () => {
                 VJS Headphones
               </Link>  
             </p>
-            <button type="button" className="cart-icon" onClick="">
+            <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
                 <AiOutlineShopping />
                 <span className="cart-item-qty">
-                    1
+                    {totalQuantities}
                 </span>
             </button>
+            { showCart && <Cart />}
         </div>
     )
 }
