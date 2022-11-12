ed2bf74a54b612242814919c99867d76326a6e49
VJS <minsung_96@naver.com>
2022-11-12 23:58:31 +0900

    Pay with stripe backend

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -2,14 +2,33 @@ import React, { useRef } from 'react'
 import Link from 'next/link'
 import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from 'react-icons/ai'
 import { TiDeleteOutline } from 'react-icons/ti'
-import { Toast } from 'react-hot-toast'
+import toast, { Toast } from 'react-hot-toast'
 
 import { useStateContext } from '../context/StateContext'
 import { urlFor } from '../lib/client'
+import getStripe from '../lib/getStripe'
 
 const Cart = () => {
     const cartRef = useRef()
     const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuantity } = useStateContext()
+    
+    const handleCheckOut = async () => {
+        const stripe = await getStripe()
+        const response = await fetch('/api/stripe', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(cartItems)
+        })
+
+        if(response.statusCode === 500) return
+
+        const data = await response.json()
+        toast.loading('Redirecting...')
+        stripe.redirectToCheckout({ sessionID: data.id })
+    }
+    
     return (
         <div className="cart-wrapper" ref={cartRef}>
             <div className="cart-container">
@@ -98,7 +117,7 @@ const Cart = () => {
                             </h3>
                         </div>
                         <div className="btn-container">
-                            <button type="button" className="btn" onClick="">
+                            <button type="button" className="btn" onClick={handleCheckOut}>
                                 Pay With Stripe
                             </button>
                         </div>
