0c083fd117fa090be61bf979e89cc99888d996d2
VJS <minsung_96@naver.com>
2022-11-13 23:56:48 +0900

    Stripe Payment function working

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,4 +1,6 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+import Stripe from 'stripe';
+
+const stripe = new Stripe(process.env.NEX_PUBLIC_STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
@@ -12,20 +14,32 @@ export default async function handler(req, res) {
           { shipping_rate: 'shr_1M3KzQDo8g5ejs3lJ182JfP5' },
           { shipping_rate: 'shr_1M3L0UDo8g5ejs3lyMFZeRkq' }
         ],
-        line_items: [
-          {
-            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-            price: '{{PRICE_ID}}',
-            quantity: 1,
-          },
-        ],
-        mode: 'payment',
+        line_items: req.body.map((item) => {
+          const img = item.image[0].asset._ref
+          const newImage = img.replace('image-', 'https://cdn.sanity.io/images/pfci7e3e/production/').replace('-webp', '.webp')
+          
+          return {
+            price_data: {
+              currency: 'krw',
+              product_data: {
+                name:item.name,
+                images: [newImage]
+              },
+              unit_amount: item.price* 100
+            },
+            adjustable_quantity: {
+              enabled: true,
+              minimum: 1
+            },
+            quantity: item.quantity
+          }
+        }),
         success_url: `${req.headers.origin}/?success=true`,
         cancel_url: `${req.headers.origin}/?canceled=true`,
       }
       // Create Checkout Sessions from body params.
       const session = await stripe.checkout.sessions.create(params);
-      res.redirect(303, session.url);
+      res.status(200).json(session)
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
     }
